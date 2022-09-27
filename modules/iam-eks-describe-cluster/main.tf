479d3eb06f44ad6044f4119b19b8b96cb1736279
nimblenitin <naidunitin1@gmail.com>
2022-09-27 19:49:59 +0530

    fix

--- modules/iam-eks-describe-cluster/main.tf
+++ modules/iam-eks-describe-cluster/main.tf
@@ -1,8 +1,8 @@
 data "aws_iam_policy_document" "policy" {
   statement {
-    sid = "S3"
-    Action   = ["eks:DescribeCluster"]
-    Resource = "*"
+    sid = "eks"
+    actions   = ["eks:DescribeCluster"]
+    resources = ["*"]
   }
 }
 
--- modules/iam-role-for-service-accounts-eks/main.tf
+++ modules/iam-role-for-service-accounts-eks/main.tf
@@ -1,4 +1,4 @@
-module "iam_eks_role" {
+module "iam_iam-role-for-service-accounts-eks" {
   source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version   = "~> 4.12"
 }
