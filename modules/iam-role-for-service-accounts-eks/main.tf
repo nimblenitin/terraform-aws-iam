2aa75663019a48bd50396e8c348226f963f8f506
nimblenitin <naidunitin1@gmail.com>
2022-09-28 22:01:27 +0530

    fix

--- modules/iam-role-for-service-accounts-eks/main.tf
+++ modules/iam-role-for-service-accounts-eks/main.tf
@@ -1,4 +1,7 @@
 module "iam_iam-role-for-service-accounts-eks" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 4.12"
+  
+  role_name_prefix      = var.role_name_prefix
+  attach_vpc_cni_policy = var.attach_vpc_cni_policy
+  vpc_cni_enable_ipv4   = var.vpc_cni_enable_ipv4
 }
