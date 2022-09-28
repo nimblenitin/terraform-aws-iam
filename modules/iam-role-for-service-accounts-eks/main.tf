b07c463f8ce9ac32d9f8d549341d7427ee396734
nimblenitin <naidunitin1@gmail.com>
2022-09-28 21:30:04 +0530

    fix

--- modules/iam-role-for-service-accounts-eks/main.tf
+++ modules/iam-role-for-service-accounts-eks/main.tf
@@ -1,4 +1,4 @@
-module "vpc_cni_irsa" {
+module "iam_iam-role-for-service-accounts-eks" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 4.12"
 }
